a66d85624cfec6fdd501861c7dffab8cd89aefa4
Frank <zhwuzhuo@gmail.com>
2017-06-13 13:33:26 +0800

    SpringBoot 升级到 1.5.4

--- src/main/java/lok/tar/app/config/SecurityConfig.java
+++ src/main/java/lok/tar/app/config/SecurityConfig.java
@@ -2,11 +2,14 @@ package lok.tar.app.config;
 
 import lok.tar.app.service.security.MySQLUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,30 +20,21 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private MySQLUserDetailsService userDetailsService;
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        // @formatter:off
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/noauth/**").permitAll()
-//                .antMatchers("/users/create").permitAll()
-//
-//                // 为 Swagger2 增加规则 TODO 这种方式不是最优秀的，是否可以通过其他方式解决？
-//                .antMatchers("/swagger-resources/**").permitAll()
-//                .antMatchers("/webjars/**").permitAll()
-//                .antMatchers("/swagger-ui.html").permitAll()
-//                .antMatchers("/v2/api-docs").permitAll()
-//
-//                .anyRequest().authenticated()
-//                .and().httpBasic()
-//                .and().csrf().disable();
-//        // @formatter:on
-//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and().csrf().disable();
+        // @formatter:on
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
